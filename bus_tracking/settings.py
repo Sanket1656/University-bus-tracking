81aef6607db7bd61b22130018a54acfa332fe201
Sanket1656 <kanjariyasanket36@gmail.com>
2025-04-01 13:01:19 +0530

    Added Pro\e for deployment

--- bus_tracking/settings.py
+++ bus_tracking/settings.py
@@ -97,12 +97,22 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+
+if os.getenv("RAILWAY_ENVIRONMENT"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": "/data/db.sqlite3",  # This makes SQLite persistent on Railway
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
@@ -159,7 +169,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-import os
-if os.getenv("RAILWAY_ENVIRONMENT"):
-    DATABASES["default"]["NAME"] = "/data/db.sqlite3"
 
